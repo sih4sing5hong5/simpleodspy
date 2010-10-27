a6e555cc0692516b87962aed7bcf50050b24fe54
kzamir <kzamir@4d0e5c55-9bc3-48b4-aca0-42c824e7a149>
2010-10-27 05:44:37 +0000

    init project
    git-svn-id: http://svn.code.sf.net/p/simple-odspy/code@1 4d0e5c55-9bc3-48b4-aca0-42c824e7a149

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+
+setup(name='simpleodspy',
+      version='0.1',
+      description='Simple spreadsheet class with ods export',
+      author='Yaacov Zamir',
+      author_email='kzamir@walla.co.il',
+      url='http://simple-odspy.sourceforge.net/',
+      packages=['simpleodspy'],
+     )
+     
