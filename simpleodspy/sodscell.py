3e553a00dfd4fd4f62a871f27e05c03e7dcdc9ce
kzamir <kzamir@4d0e5c55-9bc3-48b4-aca0-42c824e7a149>
2010-10-27 11:35:38 +0000

    add initial cell and table classes
    git-svn-id: http://svn.code.sf.net/p/simple-odspy/code@2 4d0e5c55-9bc3-48b4-aca0-42c824e7a149

---
+++ simpleodspy/sodscell.py
@@ -0,0 +1,143 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+
+# Copyright (C) 2010 Yaacov Zamir (2010) <kzamir@walla.co.il>
+# Author: Yaacov Zamir (2010) <kzamir@walla.co.il>
+
+class SodsCell:
+	def __init__(self):
+		''' init and set default values for cell elements '''
+		
+		# TextProperties
+		self.font_size = "12pt"
+		self.font_family = "sans-serif"
+		self.color = "#000000"
+		
+		# TableCellProperties
+		self.background_color = "#ffffff"
+		self.border_top = "none"
+		self.border_bottom = "none"
+		self.border_left = "none"
+		self.border_right = "none"
+		
+		# TableCell
+		self.text = ""
+		self.value_type = "string"
+		self.value = None
+		self.formula = None
+		self.date_value = None
+		
+		# Map
+		self.condition = None
+		self.condition_state = False
+		self.condition_color = "#0000ff"
+		self.condition_background_color = "#ff9999"
+		
+	def exportHtml(self, i = 0, j = 0):
+		''' export cell data as html table cell '''
+		
+		# if condition state is true use condition style
+		# we assume condition_state is up to date
+		color = [self.color, self.condition_color][self.condition_state]
+		background_color = [self.background_color, self.condition_background_color][self.condition_state]
+		
+		# adjust values for html
+		font_size = self.font_size.replace('pt', 'px')
+		border_top = self.border_top.replace('pt', 'px')
+		border_bottom = self.border_bottom.replace('pt', 'px')
+		border_left = self.border_left.replace('pt', 'px')
+		border_right = self.border_right.replace('pt', 'px')
+		
+		# create cell string
+		# we assume text is up to date
+		out = '''
+<td style="color:{0}; font-family:'{1}'; font-size:{2}; 
+		background-color:{3}; 
+		border-top:{4}; border-bottom:{5}; 
+		border-left:{6}; border-right:{7}; ">
+	{8}
+</td>'''.format(color, self.font_family, font_size,
+				background_color, 
+				border_top, border_bottom, border_left, border_right,
+				self.text)
+		
+		return out
+	
+	def exportXml(self, i = 0, j = 0):
+		''' export cell data as html table cell '''
+		
+		# create cell string
+		out = '''
+<cell>
+	<i>{0}</i>
+	<j>{1}</j>
+	
+	<color>{2}</color>
+	<font_family>{3}</font_family>
+	<font_size>{4}</font_size>
+	
+	<background_color>{5}</background_color>
+	<border_top>{6}</border_top>
+	<border_bottom>{7}</border_bottom>
+	<border_left>{8}</border_left>
+	<border_right>{9}</border_right>
+	
+	<text>{10}</text>
+	<value_type>{11}</value_type>
+	<value>{12}</value>
+	<formula>{13}</formula>
+	<date_value>{14}</date_value>
+	
+	<condition>{15}</condition>
+	<condition_state>{16}</condition_state>
+	<condition_color>{17}</condition_color>
+	<condition_background_color>{18}</condition_background_color>
+</cell>
+		'''.format(i, j,
+				self.color, self.font_family, self.font_size,
+				self.background_color, 
+				self.border_top, self.border_bottom, 
+				self.border_left, self.border_right,
+				self.text, self.value_type, self.value, 
+				self.formula, self.date_value, 
+				self.condition, self.condition_state, 
+				self.condition_color, self.condition_background_color)
+		
+		return out
+
+if __name__ == "__main__":
+	c = SodsCell()
+
+	print "Test html export:"
+	print "-----------------"
+	
+	print c.exportHtml()
+	c.text = "hello world"
+	print c.exportHtml()
+	c.condition_state = True
+	print c.exportHtml()
+
+	print "Test xml export:"
+	print "-----------------"
+	
+	print c.exportXml()
+	c.text = "hello world"
+	c.value = 123.3
+	print c.exportXml()
+	c.condition_state = False
+	print c.exportXml()
+
---
+++ simpleodspy/sodstable.py
@@ -0,0 +1,254 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+
+# Copyright (C) 2010 Yaacov Zamir (2010) <kzamir@walla.co.il>
+# Author: Yaacov Zamir (2010) <kzamir@walla.co.il>
+
+from copy import deepcopy
+from xml.etree import ElementTree
+
+from sodscell import SodsCell
+
+class SodsTable:
+	def __init__(self):
+		''' init and set default values for table elements '''
+		
+		# the data table
+		self.rows = {}
+		
+		# dtd for the xml import/export
+		self.dtd= '''<!DOCTYPE table [
+<!ELEMENT table (cell*)>
+<!ELEMENT cell (i?,j?,value,cellType,bgcolor,color,size,face,bold,border)>
+<!ELEMENT i (#PCDATA)>
+<!ELEMENT j (#PCDATA)>
+<!ELEMENT value (#PCDATA)>
+<!ELEMENT cellType (#PCDATA)>
+<!ELEMENT bgcolor (#PCDATA)>
+<!ELEMENT color (#PCDATA)>
+<!ELEMENT size (#PCDATA)>
+<!ELEMENT face (#PCDATA)>
+<!ELEMENT bold (#PCDATA)>
+<!ELEMENT border (#PCDATA)>
+]>'''
+		
+		# takes table
+		self.html_format = '''<html><head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+</head><body>
+%s
+</body></html>'''
+		
+		# takes dtd, cells
+		self.xml_format = '''<?xml version="1.0" encoding="UTF-8"?>
+%s
+<table>
+%s</table>'''
+		
+	def getCellAt(self, i, j):
+		''' get the cell object in i,j '''
+		
+		# try to get the row i
+		row = self.rows.get(i, {})
+		
+		# try to get the cell j
+		return row.get(j, SodsCell())
+	
+	def setCellAt(self, i, j, new_cell):
+		''' set the cell object in i, j '''
+		
+		# try to get the row i, if row is empty create new one
+		row = self.rows.get(i, {})
+		
+		# if the value is Null, delete cell
+		if new_cell:
+			row[j] = new_cell
+			self.rows[i] = row
+		else:
+			del row[j]
+			# if we deleted the last cell in the row delete it
+			if len(row) < 1:
+				del self.rows[i]
+		
+		return
+	
+	def setAt(self, i_range, j_range, 
+			font_size = None, font_family = None, color = None, 
+			background_color = None, border_top = None,
+			border_bottom = None, border_left = None, border_right = None,
+			text = None, value_type = None,
+			value = None, formula = None,
+			date_value = None,
+			condition = None, condition_state = None,
+			condition_color = None, condition_background_color = None):
+		''' set values of cell object range in i, j ranges '''
+		
+		# we want ranges
+		if type(i_range) != type(list()):
+			i_range = [i_range]
+		
+		if type(j_range) != type(list()):
+			j_range = [j_range]
+		
+		# loop on cell range
+		for i in i_range:
+			for j in j_range:
+				self.setAtOneCell(i, j, 
+					font_size, font_family, color, 
+					background_color, border_top,
+					border_bottom, border_left, border_right,
+					text, value_type, value, formula, date_value,
+					condition, condition_state,
+					condition_color, condition_background_color)
+				
+		return
+		
+	def setAtOneCell(self, i, j, 
+			font_size = None, font_family = None, color = None, 
+			background_color = None, border_top = None,
+			border_bottom = None, border_left = None, border_right = None,
+			text = None, value_type = None,
+			value = None, formula = None,
+			date_value = None,
+			condition = None, condition_state = None,
+			condition_color = None, condition_background_color = None):
+		''' set values of one cell object in i, j '''
+		
+		# get the cell in i,j
+		c = self.getCellAt(i, j)
+				
+		if font_size:
+			c.font_size = font_size
+		if font_family:
+			c.font_family = font_family
+		if color:
+			c.color = color
+	
+		if background_color:
+			c.background_color = background_color
+		if border_top:
+			c.border_top = border_top
+		if border_bottom:
+			c.border_bottom = border_bottom
+		if border_left:
+			c.border_left = border_left
+		if border_right:
+			c.border_right = border_right
+	
+		if text:
+			c.text = text
+		if value_type:
+			c.value_type = value_type
+		if value:
+			c.value = value
+		if formula:
+			c.formula = formula
+		if date_value:
+			c.date_value = date_value
+	
+		if condition:
+			c.condition = condition
+		if condition_state:
+			c.condition_state = condition_state
+		if condition_color:
+			c.condition_color = condition_color
+		if condition_background_color:
+			c.condition_background_color = condition_background_color
+		
+		# return cell to table
+		self.setCellAt(i, j, c)
+		
+		return
+	
+	def exportHtml(self, i_max, j_max):
+		''' export table in html format '''
+		
+		# create the table element of the html page
+		out = "<table>\n"
+		
+		for i in range(1, i_max):
+			out += "<tr>\n"
+			for j in range(1, j_max):
+				out += self.getCellAt(i,j).exportHtml()
+			out += "</tr>\n"
+		out += "</table>"
+		
+		return self.html_format % out
+		
+	def exportXml(self, i_max, j_max):
+		''' export table in xml format '''
+		
+		out = ""
+		
+		for i in range(1, i_max):
+			for j in range(1, j_max):
+				out += self.getCellAt(i,j).exportXml(i,j)
+		
+		return self.xml_format % (self.dtd, out)
+		
+	def loadXml(self, xml_text):
+		''' load cells from text in xml format '''
+		
+		# get the cells elements from our xml file
+		cells = ElementTree.XML(xml_text)
+		
+		# loop on all the cells in xml file
+		for xml_cell in cells:
+			i, j = int(xml_cell[0].text), int(xml_cell[1].text)
+			
+			# set cell
+			c = SodsCell()
+			
+			c.color = xml_cell[2].text
+			c.font_family = xml_cell[3].text
+			c.font_size = xml_cell[4].text
+			
+			c.background_color = xml_cell[5].text
+			c.border_top = xml_cell[6].text
+			c.border_bottom = xml_cell[7].text
+			c.border_left = xml_cell[8].text
+			c.border_right = xml_cell[9].text
+	
+			if xml_cell[10].text:
+				c.text = xml_cell[10].text
+			c.value_type = xml_cell[11].text
+			c.value = [None, xml_cell[12].text][xml_cell[12].text == 'None']
+			c.formula = [None, xml_cell[13].text][xml_cell[13].text == 'None']
+			c.date_value = [None, xml_cell[14].text][xml_cell[14].text == 'None']
+	
+			c.condition = [None, xml_cell[15].text][xml_cell[15].text == 'None']
+			c.condition_state = eval(xml_cell[16].text)
+			c.condition_color = xml_cell[17].text
+			c.condition_background_color = xml_cell[18].text
+			
+			# insert cell to table
+			self.setCellAt(i, j, c)
+			
+	def copy(self):
+		''' return a copy of the table '''
+		
+		return deepcopy(self)
+
+if __name__ == "__main__":
+	
+	print "Test table xml load from file:"
+	print "-----------------------"
+	
+	t2 = SodsTable()
+	t2.loadXml(file("test.xml").read())
+	print t2.exportXml(6,6)
+	
