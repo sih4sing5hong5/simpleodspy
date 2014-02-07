5f6601f418df9f4776fac21866fde45494039009
Ihc <ihcaoe@gmail.com>
2014-02-07 20:41:50 +0800

    改語法無著的所在　丞宏

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@
 from distutils.core import setup
 
 setup(name='simpleodspy',
-      version='1.3.0',
+      version='1.3.1',
       description='Simple spreadsheet class with ods export',
       author='Yaacov Zamir',
       author_email='kzamir@walla.co.il',
